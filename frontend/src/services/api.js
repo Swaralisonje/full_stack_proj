53aec048a89abbc10bd1ddf1999b76ffb836c538
Swaralisonje <155979163+Swaralisonje@users.noreply.github.com>
2026-01-28 10:16:46 +0530

    Updated API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/users"
+  baseURL: "https://backend-api-cb4x.onrender.com"
 });
 
 export default API;
